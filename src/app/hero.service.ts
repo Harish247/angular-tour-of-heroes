1ea0e25c974222129b316dffe253f5e1c3487f1a
Harish Gujja <g.harish247@gmail.com>
2017-06-25 13:28:45 -0700

    creating,editing and deleting heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HeroService {
     private heroesUrl = 'api/heroes';
+    private headers = new Headers({ 'Content-Type': 'application/json' });
     constructor(private http: Http) { };
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
@@ -25,5 +26,27 @@ export class HeroService {
             .toPromise()
             .then(response => response.json().data as Hero)
             .catch(this.handleError);
+    }
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+    create(name: String): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(res => res.json().data as Hero)
+            .catch(this.handleError);
+    }
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError)
     }
 }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -10,24 +10,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes : Hero[];
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
 
   getHeroes(): void {
-    this.heroService.getHeroes().then((heroes)=>{
+    this.heroService.getHeroes().then((heroes) => {
       this.heroes = heroes;
     });
   }
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getHeroes();
   }
-  gotoDetail(): void{
-    this.router.navigate(['/details',this.selectedHero.id]);
+  gotoDetail(): void {
+    this.router.navigate(['/details', this.selectedHero.id]);
   }
-  constructor(private heroService: HeroService, private router:Router) { 
+
+  add(name: String): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      })
+  }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+  constructor(private heroService: HeroService, private router: Router) {
     // this.heroes = this.heroService.getHeroes();
   };
 }
